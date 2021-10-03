94130481e8420066353790fa1eb80e6358b51091
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-04 01:08:01 +0600

    Initial Commit

---
+++ TabView/TabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabViewApp.swift
+//  TabView
+//
+//  Created by Sopnil Sohan on 4/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

15275d10207d2fba3bd373ad4c516ecc6fba9739
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-04 01:31:27 +0600

    A simple TabView.

--- TabView/ContentView.swift
+++ TabView/ContentView.swift
@@ -8,9 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    private var img:[String] = ["1","2","3","4","5"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        TabView {
+            TabView {
+                ForEach(img, id: \.self) {img in
+                    Image(img)
+                        .resizable()
+                }
+            }
+            .tabViewStyle(PageTabViewStyle())
+        }
+        .frame(width: UIScreen.main.bounds.width - 50, height: 180, alignment: .top)
+        .cornerRadius(25)
     }
 }
 
